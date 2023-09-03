c200ba6eff509bba14856f15d86ad1c40c064982
Hafiz Minhal Ahmed <112921748+Minhalahamed@users.noreply.github.com>
2023-09-03 07:17:18 -0700

    Create hospital_data_entry.py
    Project Title: Hospital Data Entry Project
    
    Project Overview:
    The Hospital Data Entry Project is a software application designed to streamline and organize the process of entering and managing patient information within a hospital or healthcare facility. This project aims to enhance the efficiency and accuracy of data entry, ensuring that patient data is easily accessible, secure, and well-organized.
    
    Key Features:
    
    Patient Information Entry: The project provides a user-friendly interface for hospital staff to enter essential patient details, including name, age, gender, medical condition, city, and contact information.
    
    Data Validation: Robust data validation checks are implemented to ensure that accurate and consistent information is entered. This helps in maintaining data integrity.
    
    Date and Time Stamping: The system records the date and time when each patient's information is entered, assisting in tracking and auditing changes.
    
    Patient Data Storage: All patient records are stored securely within a database, making it easy to retrieve and update information as needed.
    
    Viewing and Searching: Hospital staff can easily search and view patient data for individual records or in a list format. This feature helps in quickly locating patient information when needed.
    
    Menu-Driven Interface: The project utilizes a menu-driven interface, making it intuitive and user-friendly for experienced and new users.
    
    Exit Option: Users have the option to exit the program gracefully when their data entry or retrieval tasks are complete.
    
    Benefits:
    
    Efficiency: The Hospital Data Entry Project streamlines the data entry process, reducing the chances of errors and minimizing the time required to manage patient information.
    
    Accuracy: Data validation checks ensure accurate and consistent information is entered into the system, improving data quality.
    
    Accessibility: Patient records are readily accessible, improving the hospital's ability to provide timely and effective healthcare services.
    
    Audit Trail: Date and time stamping of data entry actions provides an audit trail for accountability and tracking changes.
    
    User-Friendly: The menu-driven interface is designed with user-friendliness in mind, making it accessible to staff with varying levels of technical expertise.
    
    Target Users:
    The Hospital Data Entry Project is intended for use by hospital administrators, receptionists, nurses, and other staff responsible for managing and accessing patient data.
    
    Technology Stack:
    
    Programming Language: Python
    Database Management System: SQLite
    User Interface: Command-line Interface (CLI)
    Note: The project can be further expanded and enhanced with additional features, such as user authentication, data encryption for security, and integration with other hospital management systems, depending on the specific requirements of the healthcare facility.

---
+++ hospital_data_entry.py
@@ -0,0 +1,56 @@
+import datetime
+
+patient = []
+
+def add_patient():
+    print("\n'Indus Hospital'\n")
+    Person_name = input("Enter patient's name: ")
+    Person_age = input("Enter patient's age: ")
+    Person_gender = input("Enter patient's gender: ")
+    Person_condition = input("Enter patient's condition: ")
+    Person_city = input("Enter patient's city: ")
+    Person_phone = input("Enter patient's phone number: ")
+
+    now = datetime.datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+
+    complete = f"Name: {Person_name}\nAge: {Person_age}\nGender: {Person_gender}\nCondition: {Person_condition}\nCity: {Person_city}\nPhone: {Person_phone}\n"
+
+    if Person_name.isalpha() and Person_age.isdigit() and Person_gender.isalpha() and Person_condition.isalpha() and Person_city.isalpha() and Person_phone.isdigit():
+        print("Patient Information:")
+        print("Name =", Person_name)
+        print("Age =", Person_age)
+        print("Gender =", Person_gender)
+        print("Condition =", Person_condition)
+        print("City =", Person_city)
+        print("Phone =", Person_phone)
+        print("=" * 40)
+        patient.append(complete)
+    else:
+        print("Invalid input. Please enter valid data.")
+
+def view_patient_data():
+    if len(patient) == 0:
+        print("No patient data available.")
+    else:
+        print("Patient Data:")
+        for i in patient:
+            print(i)
+
+while True:
+    print("\n'Indus Hospital'\n")
+    print("1: To add patient:")
+    print("2: View patient's Data:")
+    print("3: Exit")
+
+    user_choice = input("\nEnter your choice: ")
+
+    if user_choice == "1":
+        add_patient()
+    elif user_choice == "2":
+        view_patient_data()
+    elif user_choice == "3":
+        print("Exiting the program. Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please enter 1, 2, or 3.")
