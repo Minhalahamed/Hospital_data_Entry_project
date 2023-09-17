ecab2d96ff34c554343c4a5f08e23716976081ad
Hafiz Minhal Ahmed <112921748+Minhalahamed@users.noreply.github.com>
2023-09-17 13:05:10 -0700

    Update hospital_data_entry.py

--- hospital_data_entry.py
+++ hospital_data_entry.py
@@ -1,56 +1,35 @@
-import datetime
+import sqlite3
+import os
 
-patient = []
-
-def add_patient():
-    print("\n'Indus Hospital'\n")
-    Person_name = input("Enter patient's name: ")
-    Person_age = input("Enter patient's age: ")
-    Person_gender = input("Enter patient's gender: ")
-    Person_condition = input("Enter patient's condition: ")
-    Person_city = input("Enter patient's city: ")
-    Person_phone = input("Enter patient's phone number: ")
-
-    now = datetime.datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-
-    complete = f"Name: {Person_name}\nAge: {Person_age}\nGender: {Person_gender}\nCondition: {Person_condition}\nCity: {Person_city}\nPhone: {Person_phone}\n"
-
-    if Person_name.isalpha() and Person_age.isdigit() and Person_gender.isalpha() and Person_condition.isalpha() and Person_city.isalpha() and Person_phone.isdigit():
-        print("Patient Information:")
-        print("Name =", Person_name)
-        print("Age =", Person_age)
-        print("Gender =", Person_gender)
-        print("Condition =", Person_condition)
-        print("City =", Person_city)
-        print("Phone =", Person_phone)
-        print("=" * 40)
-        patient.append(complete)
-    else:
-        print("Invalid input. Please enter valid data.")
-
-def view_patient_data():
-    if len(patient) == 0:
-        print("No patient data available.")
-    else:
-        print("Patient Data:")
-        for i in patient:
-            print(i)
-
+connection = sqlite3.connect("Patientdata.db")
+cursor = connection.cursor()
+# cursor.execute("CREATE TABLE Patientdata (name TEXT,age INTEGER,gender TEXT,condition TEXT,city TEXT,phone TEXT)")
+# cursor.execute("Insert Into Patientdata Values ('Aun', '25', 'male', 'Normal',' Karachi', '0312456874',) ")
+# print(cursor.execute("select * from Patientdata ").fetchall())
+# connection.commit()
 while True:
     print("\n'Indus Hospital'\n")
     print("1: To add patient:")
     print("2: View patient's Data:")
     print("3: Exit")
-
-    user_choice = input("\nEnter your choice: ")
-
-    if user_choice == "1":
-        add_patient()
-    elif user_choice == "2":
-        view_patient_data()
-    elif user_choice == "3":
+    chose = input("Enter your choice:") 
+    if chose == '1':
+       print("\n'Indus Hospital'\n")
+       person_name = input("Enter patient's name: ")
+       person_age = int(input("Enter patient's age: "))
+       person_gender = input("Enter patient's gender: ")        
+       person_condition = input("Enter patient's condition: ")
+       person_city =  input("Enter patient's city: ")
+       person_phone =int( input("Enter patient's phone number: "))
+       cursor.execute(f"Insert into Patientdata Values('{person_name}','{person_age}','{person_gender}','{person_condition}','{person_city}','{person_phone}')")
+       connecion.commit()
+    if chose == '2':
+       rows = cursor.execute("select * from Patientdata").fetchall()
+       for i in rows:
+           print(*i, sep="\t")
+    elif chose == "3":
         print("Exiting the program. Goodbye!")
         break
     else:
-        print("Invalid choice. Please enter 1, 2, or 3.")
+      print("Invalid choice. Please enter 1, 2, or 3.")
+connection.close()
